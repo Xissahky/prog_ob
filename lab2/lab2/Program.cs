eb97e408c7ea705b1fbbecda071a48a0e39d5f37
Xissahky <pkovalskiy01@gmail.com>
2023-10-19 15:45:53 +0200

    lab2

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using lab2.constructions;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var construction = new Construction(23, 45,4,6,"brik")
+            Console.WriteLine(construction.BuildMaterial);
+        }
+    }
+}
---
+++ lab2/lab2/constructions/constructions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab2.constructions
+{
+    class Construction
+    {
+        public Construction(double height, double width, int entranses, int humancapasity, string buildMaterial)
+        {
+            Height = height;
+            Width = width;
+            Entranses = entranses;
+            Humancapasity = humancapasity;
+            BuildMaterial = buildMaterial;
+
+        }
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public int Entranses { get; set; }
+        public int Humancapasity { get; set; }
+        public string BuildMaterial { get; set; }
+    }
+}
